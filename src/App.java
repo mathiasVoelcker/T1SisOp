cf8b3e30c3bbb4f52348741e295ef1f98ba59ca2
Mathias Voelcker <mathias_V94@hotmail.com>
2018-05-05 17:48:23 -0300

    criar round robin

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
+import javafx.concurrent.Task;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class App {
 
@@ -11,6 +14,31 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(configIniciais.getNumProcessos());
+        ArrayList<Processo> processos = configIniciais.getProcessos();
+        int fatiaTempo = configIniciais.getFatiaTempo();
+        ordenarProcessosPorPrioridade(processos);
+        boolean executou = true;
+        while(executou){
+            executou = false;
+            for (Processo processo: processos) {
+                if(!processo.isFinalizado()){
+                    executou = true;
+                    processo.executar(fatiaTempo);
+                }
+            }
+        }
+    }
+
+    //método bubble sort para ordenar processos de acordo com suas prioridades,
+    public static void ordenarProcessosPorPrioridade(ArrayList<Processo> processos){
+        for(int n = processos.size(); n >= 2; n--) {
+            for(int i = 0; i < (n - 1); i++){
+                if(processos.get(i).getPrioridade() > processos.get(i + 1).getPrioridade()){
+                    int aux = processos.get(i).getPrioridade();
+                    processos.get(i).setPrioridade(processos.get(i + 1).getPrioridade());
+                    processos.get(i + 1).setPrioridade(aux);
+                }
+            }
+        }
     }
 }
--- src/Processo.java
+++ src/Processo.java
@@ -2,14 +2,37 @@ public class Processo {
     private int tempoChegada;
     private int tempoExcecucao;
     private int prioridade;
+    private int etapaProcesso;
+    private boolean finalizado;
 
     public Processo(int tempoChegada, int tempoExcecucao, int prioridade) {
         this.tempoChegada = tempoChegada;
         this.tempoExcecucao = tempoExcecucao;
         this.prioridade = prioridade;
+        this.etapaProcesso = 0;
     }
 
     public Processo(){ }
+
+    public void executar(int fatiaTempo) {
+        int tempoExecutando = 0;
+        while(tempoExecutando < fatiaTempo){
+            if(this.etapaProcesso == tempoExcecucao) {
+                finalizado = true;
+                break;
+            }
+            try {
+                Thread.currentThread().sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.etapaProcesso++;
+            tempoExecutando++;
+        }
+        System.out.println(etapaProcesso);
+        System.out.println(tempoExcecucao);
+        System.out.println("-----------------");
+    }
 
     public int getTempoChegada() {
         return tempoChegada;
@@ -21,6 +44,10 @@ public class Processo {
 
     public int getPrioridade() {
         return prioridade;
+    }
+
+    public boolean isFinalizado() {
+        return finalizado;
     }
 
     public void setTempoChegada(int tempoChegada) {
